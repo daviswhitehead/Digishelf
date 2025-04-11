4273f041775528ad1ba12cde7dddccfd21317f50
Davis Whitehead <whitehead.davis@gmail.com>
2025-04-10 17:09:18 -0700

    Login styling done

--- app/pages/login.js
+++ app/pages/login.js
@@ -1,6 +1,22 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
 import AuthPage from "../components/Login";
 
 export default function Login() {
-  return <AuthPage />;
+  return (
+    <View style={styles.container}>
+      <AuthPage />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#000",
+    color: "#fff",
+    minHeight: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
