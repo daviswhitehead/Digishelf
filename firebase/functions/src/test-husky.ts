f88cdb2e4b442322e72a47af2a79a4e591db3795
Davis Whitehead <whitehead.davis@gmail.com>
2025-04-26 18:25:41 -0400

    test: add another function to test husky

--- firebase/functions/src/test-husky.ts
+++ firebase/functions/src/test-husky.ts
@@ -3,4 +3,9 @@ function testHusky(   param1:any,param2   :string,){
     var result=param1+param2;
 console.log(result)
     return     result;
+}
+
+// Adding a new function with formatting issues
+function anotherTest(x:any,y:any,){
+    return x+    y
 } 
