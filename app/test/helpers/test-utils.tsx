b98d49da13c2c350c532f9bbdafa619a2c9f7d30
Davis Whitehead <whitehead.davis@gmail.com>
2025-04-26 23:03:26 -0400

    fix: merge conflict

--- app/test/helpers/test-utils.tsx
+++ app/test/helpers/test-utils.tsx
@@ -4,20 +4,14 @@ import { RenderOptions } from '@testing-library/react';
 
 // Add any providers here
 const AllTheProviders: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return (
-    <React.Fragment>
-      {children}
-    </React.Fragment>
-  );
+  return <React.Fragment>{children}</React.Fragment>;
 };
 
-const customRender = (
-  ui: React.ReactElement,
-  options?: Omit<RenderOptions, 'wrapper'>,
-) => rtlRender(ui, { wrapper: AllTheProviders, ...options });
+const customRender = (ui: React.ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
+  rtlRender(ui, { wrapper: AllTheProviders, ...options });
 
 // re-export everything
 export * from '@testing-library/react';
 
 // override render method
-export { customRender as render }; 
+export { customRender as render };
